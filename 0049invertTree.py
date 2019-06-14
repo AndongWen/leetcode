966b56c8a024ecba99ffce3b3c0a644bb8b4ca21
AnthonyBlackJ <1303524991@qq.com>
2019-06-14 20:12:51 +0800

    2

--- 0049invertTree.py
+++ 0049invertTree.py
@@ -7,3 +7,20 @@ class Solution:
         root.right = self.invertTree(root.left)
         root.left = self.invertTree(right)
         return root
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+        queue = []
+        queue.append(root)
+        while queue:
+            curnode = queue.pop(0)
+            temp = curnode.right
+            curnode.right = curnode.left
+            curnode.left = temp
+            if curnode.left:
+                queue.append(curnode.left)
+            if curnode.right:
+                queue.append(curnode.right)
+        return root
