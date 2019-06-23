d1a6779a8f29ac95a02410a6b10c1fce79213816
AnthonyBlackJ <1303524991@qq.com>
2019-06-23 20:27:37 +0800

    2

--- 0081isAnagram.py
+++ 0081isAnagram.py
@@ -8,7 +8,7 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         res = [0] * 26
         for char in s:
-            res[ord(char)-97] += 1
+            res[ord(char)-97] += 1 # ord() 函数是 chr() 函数（对于 8 位的 ASCII 字符串）的配对函数，它以一个字符串（Unicode 字符）作为参数，返回对应的 ASCII 数值，或者 Unicode 数值。
         for char in t:
             res[ord(char)-97] -= 1
         for i in range(26):
