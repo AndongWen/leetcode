4a58b17da7b9a67616370add3d32560fdb88b92e
AnthonyBlackJ <1303524991@qq.com>
2019-06-26 23:51:42 +0800

    1

---
+++ 0091searchMatrix.py
@@ -0,0 +1,27 @@
+'''240搜索二维矩阵：
+	写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target。该矩阵具有以下特性：
+
+每行的元素从左到右升序排列。
+每列的元素从上到下升序排列。
+ 同offer第４题'''
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if not matrix:
+            return False
+        m = len(matrix) # 行
+        n = len(matrix[0]) # 列
+        i = 0
+        j = n - 1
+        while i < m and j >= 0:
+            if target == matrix[i][j]:
+                return True
+            elif target > matrix[i][j]:
+                i += 1
+            else:
+                j -= 1
+        return False
